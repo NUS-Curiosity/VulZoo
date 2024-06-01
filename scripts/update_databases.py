7e3aa66a461e5ca463d01dbe0a42b106e35a91db
rambo <rambo@matrix.virtual>
2024-06-01 20:01:12 +0800

    update database

--- scripts/update_databases.py
+++ scripts/update_databases.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python
 
+import os
+
+os.system("git pull --recurse-submodules")
+os.system("wget https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json -O ../cisa-kev-database/known_exploited_vulnerabilities.json")
 
