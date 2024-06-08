d10cabedbd6ec8a893791712ab14f155a49146b6
rambo <rambo@matrix.virtual>
2024-06-08 18:41:43 +0800

    add a script to find duplications in intermediate data

---
+++ scripts/intermediate/find_duplications.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Create a temporary file to store the hash values and filenames
+tempfile=$(mktemp)
+
+# Calculate the hash values of all files in the directory and store the results in the temporary file
+find ../../intermediate/ -type f -exec sha256sum {} \; > "$tempfile"
+
+# Find and display duplicate files
+echo "[*] Finding duplicate files in the intermediate directory"
+echo "[+] The following are identical files:"
+sort "$tempfile" | awk '{
+    hash=$1
+    file=$2
+    if (hash in seen) {
+        seen[hash] = seen[hash] "\n\t" file
+    } else {
+        seen[hash] = file
+    }
+} END {
+    for (hash in seen) {
+        split(seen[hash], files, " ")
+        if (length(files) > 1) {
+            print seen[hash]
+        }
+    }
+}'
+
+# Remove the temporary file
+rm "$tempfile"
