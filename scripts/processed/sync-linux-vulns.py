cb37587607f57971f88b19d799ab59183608bd6f
rambo <rambo@matrix.virtual>
2024-06-13 16:04:18 +0800

    update database and readme

--- scripts/processed/sync-linux-vulns.py
+++ scripts/processed/sync-linux-vulns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import json
 
 
 def ensure_dir(directory):
@@ -8,6 +9,7 @@ def ensure_dir(directory):
         os.makedirs(directory)
 
 
+cve_mail_mapping_file = "../../processed/cve-mail-mappings.json"
 src_dir = f"../../raw-data/linux-vulns-database/cve"
 dst_dir = f"../../processed/linux-vulns-database"
 sub_dirs = ["published", "rejected"]
@@ -37,3 +39,22 @@ if __name__ == "__main__":
 
             os.system(f"cp {src_year_dir}/CVE-*.json {dst_year_dir}")
             os.system(f"cp {src_year_dir}/CVE-*.mbox {dst_year_dir}")
+
+    # update cve_mail_mappings
+    with open(cve_mail_mapping_file, "r") as f:
+        cve_mail_mappings = json.load(f)
+    
+    for root, dirs, files in os.walk(dst_dir):
+        for file in files:
+            if file.endswith(".mbox"):
+                # whole path example: ../../processed/linux-vulns-database/published/2019/CVE-2019-1234.mbox
+                # path we need: linux-vulns-database/published/2019/CVE-2019-1234.mbox
+                cve_id = file.split(".")[0]
+                if cve_id not in cve_mail_mappings:
+                    cve_mail_mappings[cve_id] = []
+                rel_path = os.path.relpath(os.path.join(root, file), start="../../processed")
+                if rel_path not in cve_mail_mappings[cve_id]:
+                    cve_mail_mappings[cve_id].append(rel_path)
+    
+    with open(cve_mail_mapping_file, "w") as f:
+        json.dump(cve_mail_mappings, f, indent=4)
