dc6bcc15f9216d7efb154b84336ef9481e9fea84
rambo <rambo@matrix.virtual>
2024-06-25 14:41:37 +0800

    fix paths

--- scripts/processed/sync-oss-security.py
+++ scripts/processed/sync-oss-security.py
@@ -4,11 +4,11 @@ import os
 import json
 import re
 
-cve_mail_mapping_file = "../../relationships/rel-cve-mail.json"
+cve_mail_mapping_file = "../../processed/relationships/rel-cve-mail.json"
 raw_data_dir = "../../raw-data/oss-security-database"
 raw_data_index_file = "oss-security-msg-links.json"
 processed_data_dir = "../../processed/oss-security-database"
-manifest_file = "../../relationships/temp-oss-security-manifest.txt"
+manifest_file = "../../processed/relationships/temp-oss-security-manifest.txt"
 
 
 def ensure_dir(directory):
